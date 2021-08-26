2f0a3f2c67a0eaf6ade714afa16db91ded917719
Taijul Islam <taijulislam.st9@gmail.com>
2021-08-26 19:40:38 +0600

    Use State with array, basic

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let title = "Random Title";
+
+  const titleHandling = () => {
+    title = "Another Title";
+    console.log(title);
+  };
+
+  return (
+    <>
+      <h2>useState error example</h2>
+      <h1> {title} </h1>
+      <button className="btn" onClick={titleHandling}>
+        Change Title
+      </button>
+    </>
+  );
 };
 
 export default ErrorExample;
--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState("Random Text");
+  const textHandler = () => {
+    console.log(text);
+    if (text === "Random Text") {
+      setText("Text has been changed");
+    } else {
+      setText("Random Text");
+    }
+  };
+  return (
+    <div>
+      <h2>useState basic example</h2>
+      <h2> {text} </h2>
+      <button className="btn" onClick={textHandler}>
+        Change Title
+      </button>
+    </div>
+  );
 };
 
 export default UseStateBasics;
