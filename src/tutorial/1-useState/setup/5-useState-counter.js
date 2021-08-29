4fba5dd5fb678e3dddfa5e61a038f0e623604bea
Taijul Islam <taijulislam.st9@gmail.com>
2021-08-29 13:58:49 +0600

    Adding: Adding some UseState functionality

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const UseStateCounter = () => {
   const [value, setValue] = useState(0);
   const increaseHandler = () => {
-    setValue(value + 1);
+    setValue((prevState) => prevState + 1);
   };
   const resetHandler = () => {
     setValue(0);
