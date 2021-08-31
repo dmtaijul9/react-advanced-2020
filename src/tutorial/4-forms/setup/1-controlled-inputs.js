e67c93efac06c90dc3763b6a604a3d2d386b63ca
Taijul Islam <taijulislam.st9@gmail.com>
2021-08-31 23:17:08 +0600

    Complited: form managing

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
@@ -6,7 +6,67 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [peoples, setPeoples] = useState([]);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (!firstName && !email) {
+      console.log("Please input all the feild");
+    }
+    const newParson = {
+      firstName: firstName,
+      email: email,
+      id: new Date().getTime().toString(),
+    };
+    console.log(newParson);
+    setPeoples((peoples) => {
+      return [...peoples, newParson];
+    });
+  };
+
+  return (
+    <React.Fragment>
+      <article>
+        <form action="get" className="form" onSubmit={submitHandler}>
+          <div className="form-control">
+            <label htmlFor="firstName">First Name : </label>
+            <input
+              type="text"
+              name="firstName"
+              id="firstName"
+              value={firstName}
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="email">Email : </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.currentTarget.value);
+              }}
+            />
+          </div>
+          <button type="submit">Add Person</button>
+        </form>
+        {peoples &&
+          peoples.map((person) => {
+            return (
+              <div className="item" key={person.id}>
+                <h2> {person.firstName} </h2>
+                <h4> {person.email} </h4>
+              </div>
+            );
+          })}
+      </article>
+    </React.Fragment>
+  );
 };
 
 export default ControlledInputs;
